c2265ca9258b491b9580f1afdbbfeee567080dec
RodrigoMartins17 <rodrigocastelam07@gmail.com>
2026-07-09 02:40:07 +0100

    a

--- app/config/config.local.php
+++
@@ -1,4 +0,0 @@
-<?php
-// FICHEIRO LOCAL COM SEGREDOS — está no .gitignore e NUNCA vai para o GitHub.
-// No servidor online, define antes variáveis de ambiente com estes valores.
-define('SMTP_PASS', 'yfkejnllbhsjzsrv');
